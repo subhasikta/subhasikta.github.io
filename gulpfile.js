388b240913e51b0ef0854d2117aefbdb67f3a0fc
subhasikta <subhasikta50@gmail.com>
2020-05-19 09:52:05 +0530

    Remove existing code and add new code and files folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-
+ 
 sass.compiler = require('node-sass');
 
 // Compile scss to css
--- js/index.js
+++ js/index.js
@@ -1,49 +1,47 @@
-const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const PHONE_REGEX = /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3,4}[-\s.]?[0-9]{4,6}$/im;
+(function aosFunction() {
+    AOS.init({
+        // uncomment below for on-scroll animations to played only once
+        // once: true  
+    }); // initialize animate on scroll library
+})();
+
 
 window.onscroll = function () {
     myFunction();
 };
 
 const navBar = document.getElementById('nav-bar');
-const scrollToTop = document.getElementById('scrollToTop');
-scrollToTop.addEventListener('click', function () {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth"
-    })
-});
 
 function myFunction() {
     if (window.pageYOffset > 60) {
         navBar.classList.add("header-bg");
-        scrollToTop.classList.add('scroll-to-top-btn-img');
     }
     else {
         navBar.classList.remove("header-bg");
-        scrollToTop.classList.remove('scroll-to-top-btn-img');
     }
 }
 
-const sendButton = document.getElementById('sendButton');
-sendButton.addEventListener('click', function () {
+const mobileOpenMenu = document.getElementById('humbergerMenu');
+const mobileCloseMenu = document.getElementById('closeMenu');
+const mobileCloseMenuClick = document.getElementById('closeMenuClick');
 
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-    const emailId = document.getElementById('emailId').value;
-    const phoneNumber = document.getElementById('phoneNumber').value;
-    const message = document.getElementById('message').value;
+mobileOpenMenu.addEventListener('click', openMenuFunction);
+mobileCloseMenuClick.addEventListener('click', closeMenuFunction);
 
-    if (firstName == '' || lastName == '' || emailId == '' || phoneNumber == '' || message == '') {
-        alert('Please fill all details');
-    }
-    else if (EMAIL_REGEX.test(emailId)) {
-        alert('Please enter a valid email');
-    }
-    else if (PHONE_REGEX.test(phoneNumber)) {
-        alert('Please enter a valid phone number');
-    }
-    else
-        console.log(firstName, lastName, emailId, phoneNumber, message);
+function openMenuFunction() {
+    mobileCloseMenu.classList.add('mobile-navigation-menu-open');
+}
+
+function closeMenuFunction() {
+    mobileCloseMenu.classList.remove('mobile-navigation-menu-open');
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+        });
+        mobileCloseMenu.classList.remove('mobile-navigation-menu-open');
+    });
 });
